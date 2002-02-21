a18a3482fc18c3922e58f209562bc6a09cc62de0
Terrence Brannon <metaperl@gmail.com>
2002-02-21 20:17:16 +0000

    Import of TBONE/Chess-Mbox-1.3 from CPAN.
    gitpan-cpan-distribution: Chess-Mbox
    gitpan-cpan-version:      1.3
    gitpan-cpan-path:         TBONE/Chess-Mbox-1.3.tar.gz
    gitpan-cpan-author:       TBONE
    gitpan-cpan-maturity:     released

--- Mbox.pm
+++ Mbox.pm
@@ -8,7 +8,7 @@ require 5.005_62;
 use strict;
 use warnings;
 
-our $VERSION = '0.01';
+our $VERSION = sprintf '%s', 'q$Revision: 1.3 $' =~ /\S+\s+(\S+)\s+/ ;
 
 
 # Preloaded methods go here.
@@ -19,12 +19,14 @@ sub Parse {
 
     my $mailbox     = $C{mbox};
     my $game_dir    = $C{output_dir};
+    my $postop      = $C{post_op};
+    warn "POSTOP: $postop";
     my $temp_file   = "/tmp/chessfile$$";
 
     my $mb = Mail::MboxParser->new($mailbox, decode => 'NEVER');
 
 while (my $msg = $mb->next_message) {
-#    print $msg->header->{subject}, "\n";
+    print $msg->header->{subject}, "\n";
 
     my $pgnfile = $temp_file;
     open  T, ">$pgnfile" or die "couldnt open $pgnfile: $!";
@@ -45,6 +47,10 @@ while (my $msg = $mb->next_message) {
 	open F, ">$F" or die "couldnt open $F: $!";
 	print F $GAME;
 	close(F);
+	if ($postop) {
+	  chdir $dir;
+	  &$postop;
+	}
     }
 
 }
@@ -64,8 +70,20 @@ Chess::Mbox - write mbox files with chess games into them onto disk
 
 =head1 SYNOPSIS
 
-  use Chess::Mbox;
-  Chess::Mbox->Parse (mmbox => $M, output_dir => $O);
+ use Chess::Mbox;
+
+ sub post_op {
+    my @can = <*.can.pgn>;
+    my @pgn = grep { $_ !~ /can.pgn/ } <*.pgn>;
+    my $pgn = shift @pgn;
+    warn "PGN: $pgn CAN: @can";
+   system "/Users/metaperl/bin/annotate.pl $pgn" unless @can;
+ }
+
+ $M = '/Users/metaperl/Library/Mail/Mailboxes/Chess/Games.mbox/mbox';
+ $O = '/Users/metaperl/Documents/Chess/games/tmp';
+ Chess::Mbox->Parse (mbox => $M, output_dir => $O, post_op => \&post_op);
+
 
 =head1 DESCRIPTION
 
@@ -73,6 +91,8 @@ This was a script lying on my disk that I thought would be useful to others. It 
 takes a Unix mbox file and assumes each message is a chess game and writes to a directory
 with first directory == white and directory below that == black and the file name == date + time
 of match... after all you will have many rematches with a certain person. :)
+
+It also will run a C<post_op> subroutine to do something with each 
 
 =head2 EXPORT
 
